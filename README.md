4b8cbbcbdd855966ed75a6a564fe53a765daba79
lantern77 <peter_hoang2@hotmail.com>
2016-07-09 19:45:26 -0400

    initial commit empty app

---
+++ README.md
@@ -0,0 +1,2 @@
+# Angular2Test
+Testing Angular 2 on Github Pages
---
+++ app/main.js
@@ -0,0 +1,7 @@
+// Bootstrap piece
+(function(app) {
+    document.addEventListener('DOMContentLoaded', function() {
+        ng.platformBrowserDynamic.bootstrap(app.AppComponent);
+    });
+
+})(window.app || (window.app = {}))
